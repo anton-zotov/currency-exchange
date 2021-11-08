2b4e8f5b43f62605a9aa90dea6976accc01890be
Anton Zotov <anton.a.zotov@gmail.com>
2021-11-08 16:07:10 +0100

    add error boundary

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import DataProvider from '../DataProvider';
+import ErrorBoundary from '../ErrorBoundary';
 import ExchangePage from '../ExchangePage';
 import { Container } from './style';
 
 function App() {
     return (
-        <Container>
-            <DataProvider>
-                <ExchangePage></ExchangePage>
-            </DataProvider>
-        </Container>
+        <ErrorBoundary>
+            <Container>
+                <DataProvider>
+                    <ExchangePage></ExchangePage>
+                </DataProvider>
+            </Container>
+        </ErrorBoundary>
     );
 }
 
